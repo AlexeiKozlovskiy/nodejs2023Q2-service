3f8d48ba6f64064755aa130ffc82bc9ca30d5174
AlexeiKozlovskiy <gekleberefin@gmail.com>
2023-07-27 17:23:33 +0300

    feat: add dotenv logic

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+const DEFAULT_PORT = 4000;
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const PORT = Number(process.env.PORT) || DEFAULT_PORT;
+  await app.listen(PORT);
 }
 bootstrap();
